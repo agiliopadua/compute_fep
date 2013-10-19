92b315c729c84fae5386b3c8da4496be4e0c543b
Agilio Padua <agilio.padua@gmail.com>
2013-10-19 20:23:18 +0200

    Improved trapfep.py

--- trapfep.py
+++ trapfep.py
@@ -4,8 +4,8 @@
 import sys, math
 
 if len(sys.argv) < 3:
-    print "usage: trapfep.py temperature hderiv"
-    print "output of compute fep read from stdin"
+    print "Integrate compute fep results using the trapezoidal rule"
+    print "usage: trapfep.py temperature hderiv < fep.lmp"
     sys.exit()
 
 rt = 0.008314 * float(sys.argv[1])
@@ -14,24 +14,22 @@ hderiv = float(sys.argv[2])
 line = sys.stdin.readline()
 while line.startswith("#"):
     line = sys.stdin.readline()
+
+v = 1.0
 tok = line.split()
 if len(tok) == 4:
     v = float(tok[3])
-else:
-    v = 1
 lo = -rt * math.log(float(tok[2]) / v)
     
-i = 1
+i = 0
 sum = 0.0
 for line in sys.stdin:
     tok = line.split()
     if len(tok) == 4:
         v = float(tok[3])
-    else:
-        v = 1
     hi = - rt * math.log(float(tok[2]) / v)
     sum += (hi + lo) / (2 * hderiv)
     lo = hi
     i += 1
 
-print sum / i
+    print sum / i      # int_0^1: divide by i == multiply by delta
