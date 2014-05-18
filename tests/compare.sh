e231aeb682784e593526d3cb221cec0540d8d7c8
Agilio Padua <agilio.padua@gmail.com>
2014-05-18 21:19:46 +0200

    Added tests to soft core CHARMM pair style

--- tests/compare.sh
+++ tests/compare.sh
@@ -5,7 +5,7 @@ DIFFOPT=numbers
 
 TSTSOFT="lj lj00 ljsoft-lam00 ljsoft-lam05 ljsoft-lam10 ljsoft-ovrl-lam10"\
 " tip4p tip4p00 tip4psoft-lam00 tip4psoft-lam05 tip4psoft-lam10"\
-" tip4psoft-ovrl-lam10"
+" tip4psoft-ovrl-lam10 charmm charmmsoft-lam10"
 
 TSTFEP="fepNVT fepNPT"
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -10,7 +10,7 @@ STEPS=300
 
 TSTSOFT="lj lj00 ljsoft-lam00 ljsoft-lam05 ljsoft-lam10 ljsoft-ovrl-lam10"\
 " tip4p tip4p00 tip4psoft-lam00 tip4psoft-lam05 tip4psoft-lam10"\
-" tip4psoft-ovrl-lam10"
+" tip4psoft-ovrl-lam10 charmm charmmsoft-lam10"
 
 TSTFEP="fepNVT fepNPT"
 
