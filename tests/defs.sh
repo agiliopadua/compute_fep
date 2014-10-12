d4791cb28590d8e36111cad4e9a657894e4b3f83
Agilio Padua <agilio.padua@gmail.com>
2014-10-12 00:03:42 -0400

    Updated tests

--- tests/defs.sh
+++ tests/defs.sh
@@ -5,7 +5,7 @@ STEPS=300
 DIFFEXE=lmp_diff
 DIFFOPT=numbers
 
-REFDIR=ref-icms
+REFDIR=ref
 
 TSTSOFT="lj lj00 ljsoft-lam00 ljsoft-lam05 ljsoft-lam10 ljsoft-ovrl-lam10"\
 " tip4p tip4p00 tip4psoft-lam00 tip4psoft-lam05 tip4psoft-lam10"\
